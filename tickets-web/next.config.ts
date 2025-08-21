a488104935fb1c582f827f319591c57fc4a2e665
Sven Patrick Meier <sven.patrick.meier@gmail.com>
2025-08-21 19:04:25 +0200

    chore: adjust next.config with dynamic proxy settings

--- tickets-web/next.config.ts
+++ tickets-web/next.config.ts
@@ -1,12 +1,17 @@
 import type { NextConfig } from "next";
 
+const backendHost = process.env.NEXT_PUBLIC_API_HOST || "be-tickets";
+const backendPort = process.env.NEXT_PUBLIC_API_PORT || "8000";
+
 const nextConfig: NextConfig = {
+  output: "standalone",
+  eslint: { ignoreDuringBuilds: true },
   trailingSlash: true,
   async rewrites() {
     return [
       {
         source: "/api/:path*/",
-        destination: "http://localhost:1234/api/:path*/",
+        destination: `http://${backendHost}:${backendPort}/api/:path*/`,
       },
     ];
   },
